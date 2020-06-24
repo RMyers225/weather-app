77501b76d790907e7e72ca253bebab4207db528d
RMyers225 <62179690+RMyers225@users.noreply.github.com>
2020-06-24 17:44:31 -0400

    added values to city & country

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import Weather from "./components/Weather"
 const API_KEY = "7c7766789fb936fde40b99a50d06b50d";
 
 class App extends React.Component {
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/
-    weather?q=Atlanta,ga&appid=${API_KEY}&units=metric`);
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/
+    weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data);
 
@@ -18,7 +21,7 @@ class App extends React.Component {
     return (
       <div>
         <Titles />
-        <Form getWeather={this.getWeather}/>
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
